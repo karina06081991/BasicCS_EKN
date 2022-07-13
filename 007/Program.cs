a2cf4ba5495b01839113db09a2c2320d047f323e
Egorova Karina <e.karina@mail.ru>
2022-07-13 17:07:47 +0300

    006

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, является ли число четным
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a%2==0)
+{
+    Console.WriteLine("a четное число");
+}
+else
+{
+    Console.WriteLine("a нечетное число");
+}
