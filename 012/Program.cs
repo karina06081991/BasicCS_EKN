6736a07af9f0806e6a99e9d65c6fcb133402035e
Egorova Karina <e.karina@mail.ru>
2022-07-13 23:12:19 +0300

    012

---
+++ 012/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводится целое число из диапазона [10,99]. Показать наибольшую цифру числа
+
+int a;
+int b,c;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a/10;
+c=a%10;
+if (b>c)
+{
+    Console.WriteLine($"{b}");
+}
+else
+{
+    Console.WriteLine($"{c}");
+}
