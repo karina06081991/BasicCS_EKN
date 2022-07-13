eb7c7a37d194089c3d8ad61d92a75cc15f3bb6a6
Egorova Karina <e.karina@mail.ru>
2022-07-13 23:11:46 +0300

    007

---
+++ 006/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления значения функции y=sin(a)
+
+/*double x,y;
+x=3.14;
+y=Math.Sin(x);
+Console.WriteLine($"sin({x})={y}");*/
+
+double Sin(double x)
+{
+    return Math.Sin(x);
+}
+
+double y=Sin(3.14);
+Console.WriteLine(y);
