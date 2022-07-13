20683de8afac5dd4240ca04e7f1e591bb4ca8246
Egorova Karina <e.karina@mail.ru>
2022-07-13 23:12:35 +0300

    015

---
+++ 015/Program.cs
@@ -0,0 +1,12 @@
+﻿// С клавиатуры вводится целое число. Вывести тертью цифру числа или сообщить, что ее нет
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b;
+b=a/10/10%10;
+Console.WriteLine($"{b}");
+if (a<100)
+{
+    System.Console.WriteLine("третьей цифры в числе нет");
+}
