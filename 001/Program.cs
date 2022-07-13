369ac17850ecc3f4fb31688082bd439c07766182
Egorova Karina <e.karina@mail.ru>
2022-07-13 17:05:58 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,13 @@
+﻿//С клавиатуры вводится целое число. Вывести квадрат числа
+
+double a;
+double b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+b=a*a;
+Console.WriteLine($"{a}^2={b}");
+
+
+/*string? s;
+s=Console.ReadLine();
+System.Console.WriteLine(s);*/
