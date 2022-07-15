b0ed127ed62d5e3ccfe41ef2fd70e3b6ac37a122
Egorova Karina <e.karina@mail.ru>
2022-07-16 01:59:49 +0300

    032

---
+++ 032/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] t;
+t=new int[8];
+
+for(int i=0;i<t.Length;i++)
+   t[i]=i*10;
+for(int i=0;i<t.Length;i++)
+   System.Console.Write($"a[{i}]={t[i]} ");
