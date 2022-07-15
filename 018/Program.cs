1d33f8cd4e799f111284831a91cfa573a4e0aa3f
Egorova Karina <e.karina@mail.ru>
2022-07-16 01:57:54 +0300

    018

---
+++ 018/Program.cs
@@ -0,0 +1,28 @@
+﻿// По двум заданным числам проверить является ли одно квадратом другого. Используйте подпрограмму.
+
+bool Check(int a, int b)
+{
+    return a*a==b;
+}
+
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+if (Check(a,b))
+{
+    Console.WriteLine("b является квадратом a");
+}
+else
+{
+    if (Check(b,a))
+    {
+        Console.WriteLine("a является квадратом b");
+    }
+    else 
+    {
+        Console.WriteLine("числа не являются квадратами друг друга");
+    }
+}
+
