609828c9c8a143d6acf1d02a1558f5d1f3154325
Egorova Karina <e.karina@mail.ru>
2022-07-16 01:59:22 +0300

    028

---
+++ 028/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе
+
+
+int CountDigits(int N)
+{
+    if (N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+       k++;
+       N=N/10;
+    }
+    return k;
+}
+
+System.Console.WriteLine(CountDigits(0));
