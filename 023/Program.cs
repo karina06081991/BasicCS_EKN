a6f66b2f6fd11746a59009be3fb504fe11f9041f
Egorova Karina <e.karina@mail.ru>
2022-07-16 01:58:45 +0300

    023

---
+++ 023/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти расстояние между двумя точками в пространстве 2D
+
+int x1=5;
+int x2=10;
+int y1=7;
+int y2=18;
+double a,b,c;
+a=x2-x1;
+b=y2-y1;
+c=Math.Sqrt(a*a+b*b);
+System.Console.WriteLine(c);
