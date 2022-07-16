86ad2ef8b212555d3d0832b3321f731b0a0030d2
Egorova Karina <e.karina@mail.ru>
2022-07-17 00:48:04 +0300

    013

---
+++ 013/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры
+
+int n=Convert.ToInt32(Console.ReadLine());
+int d0=n%10;
+int d1=n/10%10;
+int d2=n/100;
+int n1=d2*10+d0;
+System.Console.WriteLine($"{n1}");
