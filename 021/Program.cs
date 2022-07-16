f3de59764163b252cdab33700e86e5793da1b32c
Egorova Karina <e.karina@mail.ru>
2022-07-16 10:25:12 +0300

    021

---
+++ 021/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("введите номер четверти (от 1 до 4)  ");
+int a=Convert.ToInt32(Console.ReadLine());
+if(a==1)
+{
+    System.Console.WriteLine("x>0 & y>0");
+}
+if(a==2)
+{
+    System.Console.WriteLine("x<0 & y>0");
+}
+if(a==3)
+{
+    System.Console.WriteLine("x<0 & y<0");
+}
+if(a==4)
+{
+    System.Console.WriteLine("x>0 & y<0");
+}
