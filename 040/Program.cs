468ea142bbac9b3cd1a57573ea02ac8aa406ed74
Egorova Karina <e.karina@mail.ru>
2022-07-22 15:47:54 +0300

    040

---
+++ 040/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задан массив a из N элементов. Заполнить массив случайными числами от 1 до 10. Создайте массив, который является 
+// произведением пар чисел в одномерном масссиве a. Парой считается первый и последний элемент, второй и предпоследний и т.д.
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(1,11);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int[] b=new int[a.Length/2];
+for(int i=0;i<a.Length/2;i++)
+    b[i]=a[i]*a[a.Length-i-1];
+System.Console.WriteLine();
+for(int i=0;i<b.Length;i++)
+    System.Console.Write($"{b[i],4}");
