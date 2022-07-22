b1f8be0bc0781add46562f7f59a59f20ec76d676
Egorova Karina <e.karina@mail.ru>
2022-07-22 15:46:46 +0300

    036

---
+++ 036/Program.cs
@@ -0,0 +1,17 @@
+﻿// определить, присутствует ли в заданном массиве некоторое число
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+
+
+int f=2;
+//a[3]=f;
+for(int i=0;i<a.Length;i++) 
+    System.Console.Write($"{a[i],4}");
+int j=0;
+while (j<a.Length && a[j]!=f) j++;
+if (j==a.Length) System.Console.WriteLine("\nnot found");
+else System.Console.WriteLine($"\nElement {f} finded at {j}");
