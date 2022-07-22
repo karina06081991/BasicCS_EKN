2014431591391fa2ff0f634036d1f8b066ae02f0
Egorova Karina <e.karina@mail.ru>
2022-07-22 15:47:38 +0300

    039

---
+++ 039/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int s=0;
+for(int i=1;i<a.Length;i=i+2)
+    s=s+a[i];
+
+System.Console.WriteLine($"\n{s}");
