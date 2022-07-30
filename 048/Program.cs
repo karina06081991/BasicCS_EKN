faffdb324255bcf64ff736e7e19512fc4e384a31
Egorova Karina <e.karina@mail.ru>
2022-07-30 22:53:05 +0300

    048

---
+++ 048/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу копирования массива
+
+int[] a;
+Init(out a,12,-9,9);
+Print(a);
+
+int[] b;
+Clone(a,out b);
+System.Console.WriteLine();
+Print(b);
+
+
+void Clone(int[] a,out int[] b)
+{
+    b=(int[])a.Clone();
+}
+
+void Init(out int[] t, int Length,int min=0,int max=10)
+{
+    t=new int[Length];
+    Random random=new Random();
+    for(int i=0;i<t.Length;i++)
+        t[i]=random.Next(min,max+1);
+}
+
+void Print(int[] t)
+{
+    for(int i=0;i<t.Length;i++)
+    System.Console.Write($"{t[i],4} ");
+}
