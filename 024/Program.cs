8ebbb2d989bfbf58224dffdd9b700f34f84ed629
Egorova Karina <e.karina@mail.ru>
2022-07-30 22:51:14 +0300

    024

---
+++ 024/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+int i=1;
+int N=30;
+int k;
+
+while(i<N)
+{
+    k=i*i;
+    System.Console.Write($"{i}  {k} ");
+    System.Console.WriteLine();
+    i++;
+}
--- 032/Program.cs
+++ 032/Program.cs
@@ -1,9 +1,19 @@
 ﻿// Задать массив из 8 элементов и вывести их на экран
 
 int[] t;
-t=new int[8];
+Init(out t,8,0,10);
+Print(t);
 
-for(int i=0;i<t.Length;i++)
-   t[i]=i*10;
-for(int i=0;i<t.Length;i++)
-   System.Console.Write($"a[{i}]={t[i]} ");
+   void Init(out int[] t, int Length,int min,int max)
+   {
+      t=new int[Length];
+      Random random=new Random();
+      for(int i=0;i<t.Length;i++)
+       t[i]=random.Next(min,max+1);
+   }
+
+   void Print(int[] t)
+   {
+      for(int i=0;i<t.Length;i++)
+      System.Console.Write($"a[{i}]={t[i]} ");
+   }
