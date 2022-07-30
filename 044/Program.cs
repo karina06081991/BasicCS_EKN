4f1f01f7830b7acf1e8982072e1ac588d5469286
Egorova Karina <e.karina@mail.ru>
2022-07-30 22:52:37 +0300

    044

---
+++ 044/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+int n=16;
+System.Console.WriteLine(DecToBin(n));
+string DecToBin(int n)
+{
+    string s="";
+    while (n!=0)
+    {
+        s=(n%2).ToString()+s;
+        n=n/2;
+    }
+    return s;
+}
