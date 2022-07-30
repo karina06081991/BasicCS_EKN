f8cc70311fbf324c25140c9fd9cdb4f6fb5160e8
Egorova Karina <e.karina@mail.ru>
2022-07-30 22:52:17 +0300

    042

---
+++ 042/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить, являются ли три числа стронами треугольника
+
+double a=2,b=4,c=3;
+System.Console.WriteLine(IsTreangle(a,b,c));
+
+bool IsTreangle(double a, double b, double c)
+{
+    return (a+b>c && b+c>a && a+c>b);
+}
+
