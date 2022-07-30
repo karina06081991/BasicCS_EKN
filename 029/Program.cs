280b3460006f1ee4eb4963ecb5e428c7ca6d7e69
Egorova Karina <e.karina@mail.ru>
2022-07-30 22:51:40 +0300

    029

---
+++ 029/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+int num, sum = 0, r;
+System.Console.WriteLine("Введите число : ");
+num = int.Parse(Console.ReadLine());
+
+while(num != 0)
+{
+    r=num%10;
+    num=num/10;
+    sum=sum+r;
+}
+
+System.Console.WriteLine($"{sum}");
