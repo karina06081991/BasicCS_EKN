4b41aeaaace7b8355af29fca1ff92b326150d6eb
Egorova Karina <e.karina@mail.ru>
2022-08-14 12:36:33 +0300

    049

---
+++ 049/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать двумерный массив m*n заполненный вещественными случайными числами
+
+int N=3;
+int M=2;
+
+double[,] a;
+
+a=new double[N,M];
+
+Random random=new Random();
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++)
+    {
+        a[i,j]=random.NextDouble();
+    }
+}
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]:F2} ");
+        System.Console.WriteLine();
+}
