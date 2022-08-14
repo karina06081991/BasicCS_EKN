6e689d0922748d0b3dc6a9f8f21ab005159d41ab
Egorova Karina <e.karina@mail.ru>
2022-08-14 12:36:21 +0300

    048.1

---
+++ 048.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать двумерный массив размером m*n заполненный целыми случайными числами
+
+int N=3;
+int M=2;
+
+int[,] a;
+
+a=new int[N,M];
+
+Random random=new Random();
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++)
+    {
+        a[i,j]=random.Next(1,11);
+    }
+}
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]} ");
+        System.Console.WriteLine();
+}
