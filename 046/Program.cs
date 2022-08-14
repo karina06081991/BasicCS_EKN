a599544a7530617b205b2519b7ef0f92c0d0c991
Egorova Karina <e.karina@mail.ru>
2022-08-14 07:40:07 +0300

    046

---
+++ 046/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+
+int N=int.Parse(Console.ReadLine());
+int[] a=new int[N];
+a[0]=0;
+a[1]=1;
+    
+for(int i=2;i<a.Length;i++)
+    a[i]=a[i-1]+a[i-2];
+
+for(int i=2;i<a.Length;i++)
+    System.Console.WriteLine($"{a[i],4}");
+
+
