752addd185390efe68edf6a8dc7e61c7dab7f9f3
Egorova Karina <e.karina@mail.ru>
2022-08-20 13:38:33 +0300

    +2

---
+++ 042A/Program.cs
@@ -0,0 +1,53 @@
+﻿// Даны две квадратные таблицы чисел. Требуется построить третью, каждый элемент которой равен сумме элементов, стоящих
+// на том же месте в 1-й и 2-й таблицах. Входные данные
+// Во входном файле записано сначала число N, затем записана первая таблица, а после нее - вторая. 
+// Элементы таблиц - числа от 0 до 100. 100>=N>=1.
+
+
+int N=3;
+int [,] a;
+int [,] b;
+int [,] c;
+a=InitA(N,N);
+b=InitA(N,N);
+c=InitA(N,N);
+System.Console.WriteLine("Таблица 1");
+Print(a);
+System.Console.WriteLine("Таблица 2");
+Print(b);
+Sum(a,b);
+System.Console.WriteLine("Таблицы 3: сумма таблиц 1 и 2");
+Print(c);
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j],5} ");
+        System.Console.WriteLine();
+    }
+}
+
+int[,] InitA(int N,int M)
+{
+    a=new int[N,M];
+
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) 
+        {
+            a[i,j]=random.Next(1,100);
+        }
+    }
+    return a;
+}
+
+void Sum(int[,] a, int[,] b)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) 
+        c[i,j] = a[i,j] + b[i,j];
+    }
+}
---
+++ 41А/Program.cs
@@ -0,0 +1,52 @@
+﻿// Дана квадратная матрица. Проверить, является ли она симметричной относительно главной диагонали
+
+
+
+int[,] a;
+a=Init(4,4);
+Print(a);
+
+bool flag = false;
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1);j++) 
+    {
+        if (a[i,j]!=a[j,i])
+            flag = false;
+        else   
+            flag = true;  
+    } 
+}
+if (flag)
+System.Console.WriteLine("матрица несимметрична относительно главной диагонали");
+else
+System.Console.WriteLine("матрица симметрична относительно главной диагонали");
+
+            
+System.Console.WriteLine();
+
+
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j],4} ");
+            System.Console.WriteLine();
+    }
+}
+
+int[,] Init(int N, int M)
+{
+    a=new int[N,M];
+    Random random=new Random();
+
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++)
+        {
+            a[i,j]=random.Next(1,3);
+        }
+    }
+    return a;
+}
