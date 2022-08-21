74470479fb8ad6d9c0ecdc2cb821bea8563f8b2b
Egorova Karina <e.karina@mail.ru>
2022-08-21 14:23:00 +0300

    21/08

---
+++ 054А/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] a;
+a=Init(3,4);
+Print(a);
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    for(int j=0;j<a.GetLength(1)-1;j++) 
+    {
+        for(int z=0;z<a.GetLength(1)-1;z++)
+        {
+            if (a[i,z]<a[i,z+1])
+            {
+            int temp=0;
+            temp=a[i,z];
+            a[i,z]=a[i,z+1];
+            a[i,z+1]=temp;
+            }
+        }
+    }
+}
+System.Console.WriteLine();
+
+Print(a);
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j],4} ");
+            System.Console.WriteLine();
+    }
+}
+
+int[,] Init(int N, int M)
+{
+    a=new int[N,M];
+    Random random=new Random();
+
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++)
+        {
+            a[i,j]=random.Next(1,11);
+        }
+    }
+    return a;
+}
---
+++ 056A/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей
+// суммой элементов. Программа считает сумму элементов в каждой строке и выдает номер строки с наименьшей суммой элементов.
+
+int[,] a;
+a=Init(5,3);
+Print(a);
+int minsum=100;
+
+int indexLinemin=0;
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    int sum=0;
+    for(int j=0;j<a.GetLength(1);j++) 
+    {
+        sum=sum+a[i,j];
+    }
+    if(sum<minsum)
+    {
+        minsum=sum;
+        indexLinemin=i;
+    }
+    
+}
+System.Console.WriteLine($"{indexLinemin+1} ");
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j],4} ");
+            System.Console.WriteLine();
+    }
+}
+
+int[,] Init(int N, int M)
+{
+    a=new int[N,M];
+    Random random=new Random();
+
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++)
+        {
+            a[i,j]=random.Next(1,11);
+        }
+    }
+    return a;
+}
